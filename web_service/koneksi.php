5b1f7e8ba655a41848070b343baa32a001542a63
Iwanbudi10 <80971074+Iwanbudi10@users.noreply.github.com>
2021-04-03 10:40:20 +0700

    Add files via upload

---
+++ web_service/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id15134514_root');
+ define('PASS','L&0hHhs_69!_te})');
+ define('DB','id15134514_restoran');
+
+ $con = new mysqli(HOST,USER,PASS,DB) or die('Unable to Connect');
